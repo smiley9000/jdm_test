ca26f5a722b4533b9058316fd3bfb955a751728a
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-18 08:25:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,12 @@ rm -rf packages/modules/Bluetooth
 rm -rf device/mediatek/sepolicy_vndr
 sudo apt update
 sudo apt install -y dos2unix
-git clone https://github.com/smiley9000/android_device_samsung_a05m device/samsung/a05m
+
+git clone https://github.com/smiley9000/android_device_samsung_a05m -b evo_x device/samsung/a05m
 git clone https://github.com/smiley9000/vendor_samsung_a05m vendor/samsung/a05m
 git clone https://github.com/smiley9000/android_frameworks_av frameworks/av
 git clone https://github.com/smiley9000/android_packages_modules_Bluetooth packages/modules/Bluetooth
-git clone https://github.com/smiley9000/android_device_lineage_sepolicy device/lineage/sepolicy
+git clone https://github.com/smiley9000/android_device_rising_sepolicy device/lineage/sepolicy
 git clone https://github.com/smiley9000/hm vendor/lineage-priv/keys
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b lineage-21 hardware/samsung_ext
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
