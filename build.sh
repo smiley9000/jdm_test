20fc1a1fd7c0565dbcca59b26905042f41f88503
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-19 12:12:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,16 +7,23 @@ rm -rf vendor/samsung/a05m
 #rm -rf device/lineage/sepolicy
 #rm -rf packages/modules/Bluetooth
 rm -rf device/mediatek/sepolicy_vndr
+rm -rf system/sepolicy
+
 sudo apt update
 sudo apt install -y dos2unix
 git clone https://github.com/smiley9000/android_device_samsung_a05m -b evo_x device/samsung/a05m
 git clone https://github.com/smiley9000/vendor_samsung_a05m vendor/samsung/a05m
+
 #git clone https://github.com/smiley9000/android_frameworks_av_rr frameworks/av
 #git clone https://github.com/smiley9000/android_packages_modules_Bluetooth_rr packages/modules/Bluetooth
 git clone https://github.com/smiley9000/android_device_rising_sepolicy device/lineage/sepolicy
+
 #git clone https://github.com/smiley9000/hm vendor/lineage-priv/keys
+
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b lineage-21 hardware/samsung_ext
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_system_sepolicy_rr system/sepolicy
+
 dos2unix device/samsung/a05m/sepolicy/private/lpm.te
 source build/envsetup.sh
 lunch lineage_a05m-ap2a-userdebug
