de5545df5e57122b7b7ecd9265ebccbe41ee5b6c
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-10-26 08:07:29 +0530

    Update ev.sh

--- ev.sh
+++ ev.sh
@@ -3,7 +3,7 @@ use#!/bin/bash
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 
 echo "--------------------------------------"
 echo "Repo init success"
@@ -115,7 +115,7 @@ rm -rf device/samsung/a05m
 rm -rf vendor/samsung/a05m
 
 #clone
-git clone https://github.com/smiley9000/android_device_samsung_a05m device/samsung/a05m
+git clone https://github.com/smiley9000/android_device_samsung_a05m -b afterlife device/samsung/a05m
 git clone https://github.com/smiley9000/vendor_samsung_a05m vendor/samsung/a05m
 git clone https://github.com/smiley9000/hm vendor/lineage-priv/keys
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b lineage-21 hardware/samsung_ext
