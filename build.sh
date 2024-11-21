f737de6f2a1c7115edb19f8c9dd91282229579b6
Smiley <59495159+smiley9000@users.noreply.github.com>
2024-11-21 12:21:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -127,7 +127,8 @@ git clone https://github.com/smiley9000/android_frameworks_opt_telephony -b line
 
 #start build
 source build/envsetup.sh
-lunch goafterlife_a05m-userdebug
+lunch goafterlife_a05m-userdebuglunch 
+goafterlife_a05m-userdebug
+
 goafterlife a05m
-make bacon
 
