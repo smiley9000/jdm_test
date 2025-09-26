3bbd15abbef03d5f2e9c7e3c4b0814afacb77133
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-09-26 21:06:28 +0530

    Update pix2.sh

--- pix2.sh
+++ pix2.sh
@@ -4,7 +4,7 @@
 repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 
 echo "--------------------------------------"
-echo "Repo init success"
+echo " Repo init success"
 echo "--------------------------------------"
 
 
@@ -12,24 +12,41 @@ echo "--------------------------------------"
 /opt/crave/resync.sh
 
 echo "--------------------------------------"
-echo "Sync success"
+echo " Sync success"
 echo "--------------------------------------"
 
+echo "--------------------------------------"
+echo " Clean Trees"
+echo "--------------------------------------"
+rm -rf  vendor/infinix/X6531
+rm -rf  device/infinix/X6531
+rm -rf device/infinix/X6531-kernel
+rm -rf NotificationShadeWindowControllerImpl.java
 
+
+echo "--------------------------------------"
+echo " Clone Trees"
+echo "--------------------------------------"
 git clone https://github.com/smiley9000/android_device_infinix_x6531 -b lineage-23.0 device/infinix/X6531
 git clone https://github.com/smiley9000/X6531_vndr -b bka-2 vendor/infinix/X6531
 git clone https://github.com/smiley9000/android_device_infinix_X6531-kernel device/infinix/X6531-kernel
 
+echo "--------------------------------------"
+echo " Clone MediaTek Dependecies"
+echo "--------------------------------------"
 git clone https://github.com/crdroidandroid/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://github.com/crdroidandroid/android_hardware_mediatek hardware/mediatek 
 
 git clone https://gitlab.com/17101443/key vendor/lineage-priv/keys
 
-wget https://github.com/smiley9000/jdm_test/raw/refs/heads/main/NotificationShadeWindowControllerImpl.java
+wget https://raw.githubusercontent.com/smiley9000/jdm_test/main/NotificationShadeWindowControllerImpl.java
 
 mv frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java NotificationShadeWindowControllerImpl.java.bk
 cp NotificationShadeWindowControllerImpl.java frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
 
+echo "--------------------------------------"
+echo " Building"
+echo "--------------------------------------"
 
 . build/envsetup.sh
 lunch lineage_X6531-bp1a-userdebug
