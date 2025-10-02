26f4cb3b10005177abc3247418a31fd8165b65df
Smiley <59495159+smiley9000@users.noreply.github.com>
2025-10-02 10:10:53 +0530

    Update pix2.sh

--- pix2.sh
+++ pix2.sh
@@ -4,14 +4,14 @@
 #rm -rf prebuilts/clang/host/linux-x86
 
 # ROM source repo
-#repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen --git-lfs
 echo "--------------------------------------"
 echo "Repo init success"
 echo "--------------------------------------"
 
 
 # Re-sync
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 echo "--------------------------------------"
 echo " Synced Successfully "
 echo "--------------------------------------"
@@ -27,13 +27,13 @@ echo "--------------------------------------"
 rm -rf  vendor/infinix/X6531
 rm -rf  device/infinix/X6531
 rm -rf device/infinix/X6531-kernel
-rm -rf NotificationShadeWindowControllerImpl.java
+#rm -rf NotificationShadeWindowControllerImpl.java
 
 
 echo "--------------------------------------"
 echo " Clone Trees"
 echo "--------------------------------------"
-git clone https://github.com/smiley9000/android_device_infinix_x6531 -b lineage-23.0 device/infinix/X6531
+git clone https://github.com/smiley9000/android_device_infinix_x6531 -b aosp device/infinix/X6531
 git clone https://github.com/smiley9000/X6531_vndr -b bka-2 vendor/infinix/X6531
 git clone https://github.com/smiley9000/android_device_infinix_X6531-kernel device/infinix/X6531-kernel
 #
@@ -46,20 +46,19 @@ git clone https://github.com/techyminati/android_vendor_mediatek_ims vendor/medi
 
 git clone https://gitlab.com/17101443/key vendor/lineage-priv/keys
 
-wget https://raw.githubusercontent.com/smiley9000/jdm_test/main/NotificationShadeWindowControllerImpl.java
-
-mv frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java STK_NotificationShadeWindowControllerImpl.java.bk
-
-cp NotificationShadeWindowControllerImpl.java frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
+#wget https://raw.githubusercontent.com/smiley9000/jdm_test/main/NotificationShadeWindowControllerImpl.java
+#mv frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java STK_NotificationShadeWindowControllerImpl.java.bk
+#cp NotificationShadeWindowControllerImpl.java frameworks/base/packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
 
 echo "--------------------------------------"
 echo " Building"
 echo "--------------------------------------"
 
 . build/envsetup.sh
-lunch lineage_X6531-bp2a-userdebug
+lunch aosp-bp2a-userdebug
 
-m evolution
+mka bacon
+
 
 
 
